5a0e37f26c350731adbf365259dd0419774cfddc
FUJISAWA-YUKIHO <fujisawa.yukiho@ezweb.ne.jp>
2026-01-15 22:57:14 +0900

    Add files via upload

---
+++ 6_1_PWM_LED/src/smc_gen/r_pincfg/Pin.c
@@ -0,0 +1,59 @@
+/*
+* Copyright (c) 2016 - 2025 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+/***********************************************************************************************************************
+* File Name        : Pin.c
+* Version          : 1.0.2
+* Device(s)        : R5F51403AxFM
+* Description      : This file implements SMC pin code generation.
+***********************************************************************************************************************/
+
+/***********************************************************************************************************************
+Pragma directive
+***********************************************************************************************************************/
+/* Start user code for pragma. Do not edit comment generated here */
+/* End user code. Do not edit comment generated here */
+
+/***********************************************************************************************************************
+Includes
+***********************************************************************************************************************/
+#include "r_cg_macrodriver.h"
+/* Start user code for include. Do not edit comment generated here */
+/* End user code. Do not edit comment generated here */
+#include "r_cg_userdefine.h"
+
+/***********************************************************************************************************************
+Global variables and functions
+***********************************************************************************************************************/
+/* Start user code for global. Do not edit comment generated here */
+/* End user code. Do not edit comment generated here */
+
+/***********************************************************************************************************************
+* Function Name: R_Pins_Create
+* Description  : This function initializes Smart Configurator pins
+* Arguments    : None
+* Return Value : None
+***********************************************************************************************************************/
+
+void R_Pins_Create(void)
+{
+    R_BSP_RegisterProtectDisable(BSP_REG_PROTECT_MPC);
+
+    /* Set MTIOC0A pin */
+    MPC.PB3PFS.BYTE = 0x01U;
+    PORTB.PMR.BYTE |= 0x08U;
+
+    /* Set MTIOC4A pin */
+    MPC.PA0PFS.BYTE = 0x01U;
+    PORTA.PMR.BYTE |= 0x01U;
+
+    /* Set MTIOC4C pin */
+    MPC.PA4PFS.BYTE = 0x03U;
+    PORTA.PMR.BYTE |= 0x10U;
+
+    R_BSP_RegisterProtectEnable(BSP_REG_PROTECT_MPC);
+}
+
